678be4dfcf8087afb5169dcd57b6c4b847105e18
Derick Bui <derickbui64@gmail.com>
2020-09-21 00:24:31 -0700

    Update Cipher1.py

--- Cipher1.py
+++ Cipher1.py
@@ -1,5 +1,20 @@
 alphabets = "abcdefghijklmnopqrstuvwxyz" # a normal alphabet
-key = "lmnopqrstuvwxyzabcdefghijk" # a modified alphabet that is shifted by 11
+key = "bcdefghijklmnopqrstuvwxyza" # a modified alphabet that is shifted by 1
+key = key[1:] + key[0]
+
+bruteForceStorage1 = [0] * 26
+bruteForceStorage2 = [0] * 26
+bruteForceStorage3 = [0] * 26
+bruteForceStorage4 = [0] * 26
+
+cipher1 = "fqjcb rwjwj vnjax bnkhj whxcq nawjv nfxdu mbvnu ujbbf nnc"
+
+cipher2 = "oczmz vmzor jocdi bnojv dhvod igdaz admno ojbzo rcvot jprvi oviyv aozmo cvooj ziejt dojig toczr dnzno jahvi fdiyv xcdzq zoczn zxjiy"
+
+cipher3 = "ejitp spawa qleji taiul rtwll rflrl laoat wsqqj atgac kthls iraoa twlpl qjatw jufrh lhuts qataq itats aittk stqfj cae"
+
+cipher4 = "iyhqz ewqin azqej shayz niqbe aheum hnmnj jaqii yuexq ayqkn jbeuq iihed yzhni ifnun sayiz yudhe sqshu qesqa iluym qkque aqaqm oejjs hqzyu jdzqa diesh niznj jayzy uiqhq vayzq shsnj jejjz nshna hnmyt isnae sqfun dqzew qiead zevqi zhnjq shqze udqai jrmtq uishq ifnun siiqa suoij qqfni syyle iszhn bhmei squih nimnx hsead shqmr udquq uaqeu iisqe jshnj oihyy snaxs hqihe lsilu ymhni tyz"
+
 message = "He who fights with monsters should look to it that he himself does not become a monster . And if you gaze long into an abyss , the abyss also gazes into you ."
 
 # this a function that will encrypt the message
@@ -10,7 +25,7 @@ def encrypt(message, key):
       cipher += key[alphabets.index(i)]
     elif (i.isupper()): # if uppercase letter is there
       cipher += key[alphabets.index(i.lower())].upper()
-    else: # if any other character is there
+    elif (i != " "): # if any other character is there beside spaces
       cipher += i
   return cipher
 
@@ -22,13 +37,33 @@ def decrypt(cipher, key):
       text += alphabets[key.index(i)]
     elif (i.isupper()): # if uppercase letter is there
       text += alphabets[key.index(i.lower())].upper()
-    else:
+    elif (i != i): # if any other character is there beside spaces
       text += i
   return text
 
-encryptedMessage = encrypt(message, key)
-decryptedMessage = decrypt(encryptedMessage, key)
+# encryptedMessage = encrypt(message, key)
+# decryptedMessage = decrypt(encryptedMessage, key)
 
 
-print(encryptedMessage)
-print(decryptedMessage)
+# print(encryptedMessage)
+# print(decryptedMessage)
+
+#brute force encrypted message 1
+for i in range(26):
+  bruteForceStorage1[i] = decrypt(cipher1, key)
+  print(bruteForceStorage1[i])
+  key = key[1:] + key[0] # change key by one
+
+print("")
+
+#brute force encrypted message 2
+for i in range(26):
+  print(decrypt(cipher2, key))
+  key = key[1:] + key[0] # change key by one
+
+print("")
+
+#brute force encrypted message 3
+for i in range(26):
+  print(decrypt(cipher3, key))
+  key = key[1:] + key[0] # change key by one
