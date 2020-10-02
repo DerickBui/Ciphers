e223cf1512c46b790d42350f55ef48e92d1688da
Derick Bui <derickbui64@gmail.com>
2020-10-02 01:22:42 -0700

    Update MainCipher.py

--- MainCipher.py
+++ MainCipher.py
@@ -1,32 +1,23 @@
-#imports library of english words
+# imports library of english words and methods
 import enchant
-#check the use of letter e and dictionary
+# imports library to use math random functions
+import random
+# check the use of letter e and dictionary
 d = enchant.Dict("en_US")
 
-alphabets = "abcdefghijklmnopqrstuvwxyz" # a normal alphabet
-key = "bcdefghijklmnopqrstuvwxyza" # a modified alphabet that is shifted by 1
+alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" # a normal alphabet
+key = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" # use for modification
 
-# Information-------------------------------------------
-# estimated frequency of letters from most to least
-mostCommon = "ETAOINSHRDLUCMWFYGPBVKXJQZ"
 
-# Most common double letter combinations
-doubleComb = {1 : "SS", 2 : "EE", 3 : "TT", 4 : "FF", 5 : "LL", 6 : "MM", 7 : "OO"}
-
-# Most common three letter combinations
-threeComb = {1 : "THE", 2 : "AND", 3 : "THA:", 4 : "ENT", 5 : "ION", 6 : "TIO", 7 : "FOR", 8 : "NDE", 9 : "HAS", 10 : "NCE", 11 : "TIS", 12 : "OFT", 13 : "MEN"}
-
-# Most common two letter combinations
-twoComb = {1 : "TH", 2 : "HE", 3 : "AN", 4 : "IN", 5 : "ER", 6 : "ON", 7 : "RE", 8 : "ED", 9 : "ND", 10 : "HA", 11 : "AT", 12 : "EN"}
-
-#For assigning letters
-subLetters = {'a':None, 'b':None, 'c':None, 'd':None, 'e':None, 'f':None, 'g':None, 'h':None, 'i':None, 'j':None, 'k':None, 'l':None, 'm':None, 'n':None, 'o':None, 'p':None, 'q':None, 'r':None, 's':None, 't':None, 'u':None, 'v':None, 'w':None, 'x':None, 'y':None, 'z':None}
-
-# length of alphabets is 26
+# length of alphabets is 26, stores sentences and keys
 bruteForceStorage1 = [0] * len(alphabets)
+bruteForceStorageKeys1 = [0] * len(alphabets)
 bruteForceStorage2 = [0] * len(alphabets)
+bruteForceStorageKeys2 = [0] * len(alphabets)
 bruteForceStorage3 = [0] * len(alphabets)
+bruteForceStorageKeys3 = [0] * len(alphabets)
 bruteForceStorage4 = [0] * len(alphabets)
+bruteForceStorageKeys4 = [0] * len(alphabets)
 
 # Ciphers---------------------------------------------
 cipher1 = "fqjcb rwjwj vnjax bnkhj whxcq nawjv nfxdu mbvnu ujbbf nnc"
@@ -39,13 +30,31 @@ cipher4 = "iyhqz ewqin azqej shayz niqbe aheum hnmnj jaqii yuexq ayqkn jbeuq iih
 
 # FUNCTIONS-------------------------------------------
 # this a function that will encrypt the message
+
+# Create a dictionary of bigrams
+bigramScore = {}
+with open("bigrams.txt") as f:
+  for i in f:
+    (onekey, value) = i.split()
+    bigramScore[onekey] = int(value)
+
+# Function that scores what best fits according to bigramScore
+def scoring(changedCipher):
+  score = 0
+  for i in range(len(changedCipher) - 1):
+    if changedCipher[i:i+2] in bigramScore:
+      score = score + bigramScore[changedCipher[i:i+2]]
+    else:
+      score = score + (-11.63)
+  return score
+
 def encrypt(message, key):
   cipher = ""
   for i in message:
     if (i in key):
       cipher += key[alphabets.index(i)]
-    elif (i.isupper()): # if uppercase letter is there
-      cipher += key[alphabets.index(i.lower())].upper()
+    elif (i.lower()): # if uppercase letter is there
+      cipher += key[alphabets.index(i.upper())].lower()
     elif (i != " "): # if any other character is there beside spaces
       cipher += i
   return cipher
@@ -56,23 +65,26 @@ def decrypt(cipher, key):
   for i in cipher:
     if (i in key):
       text += alphabets[key.index(i)]
-    elif (i.isupper()): # if uppercase letter is there
-      text += alphabets[key.index(i.lower())].upper()
+    elif (i.islower()): # if uppercase letter is there
+      text += alphabets[key.index(i.upper())].lower()
     elif (i != i): # if any other character is there beside spaces
       text += i
   return text
 
 # this function brute forces all possible combinations
-def bruteForce(cipher, key):
-  bruteForceStorage = [0] * len(alphabets) 
+def bruteForce(cipher, storeKey, key):
+  print("Caesar ciphering")
+  bruteForceStorage = [0] * len(alphabets)
   for i in range(len(alphabets)):
     bruteForceStorage[i] = decrypt(cipher, key)
-    print(str(i+1) + ". " + bruteForceStorage[i])
+    storeKey[i] = key
     key = key[1:] + key[0] # shift key by one left
+  print("Caesar ciphering finished")
   return bruteForceStorage
 
 # this function finds correct key from brute force decrypt
-def findCorrectKey(bruteForceStorage):
+def findCorrectKey(bruteForceStorage, storeKey):
+  print("Hill climb ciphering")
   markKey = 0
   finalWordCount = 0
   for i in range(len(bruteForceStorage)):
@@ -85,81 +97,164 @@ def findCorrectKey(bruteForceStorage):
       finalWordCount = wordCount
       markKey = i
 
-  print("")
-  print("Message in key " + str(markKey + 1) + ": " + bruteForceStorage[markKey])
-  print("")
+  stringAndKey = [bruteForceStorage[markKey], str(storeKey[markKey])]
+  return stringAndKey
 
-# Function that returns ordered dictionary of combination frequency by greatest to least
-def orderLetFreqDict(letterFreq):
-  incrNum = 1
-  orderedDict = {}
-  for i in reversed(sorted(letterFreq, key=letterFreq.get)):
-    orderedDict[incrNum] = i
-    incrNum = incrNum + 1
+# This function utilizes hill climbing algorithm to find key (not perfect as it relies too much on randomness)
+def hillClimb(cipher):
+  overallCounter = 0
+  overallMessage = ""
+  overallMaxScore = -99e9
+  overallMaxPlayKey = list(key)
 
-  return orderedDict
+  maxScore = -99e9
+  maxPlayKey = list(key)
 
-# Function that counts character frequency
-def oneLetFreqCheck(cipherNS):
-  charCount = {}
-  for i in cipherNS:
-    if (i in charCount):
-      charCount[i] = charCount[i] + 1
-    else:
-      charCount[i] = 1
-  return charCount
+  while overallCounter != 30:
+    
+    print("Process " + str(overallCounter + 1) + " of 30 in progress")
+    
+    random.shuffle(maxPlayKey)
 
-# Function that finds the most frequent two letters
-def twoLetFreqCheck(cipherNS):
-  threeFreq = {}
-  for i in range(len(cipherNS)-2):
-    if (cipherNS[i:i+2] in threeFreq):
-      threeFreq[cipherNS[i:i+2]] = threeFreq[cipherNS[i:i+2]] + 1
-    else:
-      threeFreq[cipherNS[i:i+2]] = 1
-  
-  return threeFreq
+    maxDecrypted = decrypt(cipher, maxPlayKey)
+    maxScore = scoring(maxDecrypted)
 
-# Function that finds the most frequent three letters
-def threeLetFreqCheck(cipherNS):
-  threeFreq = {}
-  for i in range(len(cipherNS)-2):
-    if (cipherNS[i:i+3] in threeFreq):
-      threeFreq[cipherNS[i:i+3]] = threeFreq[cipherNS[i:i+3]] + 1
-    else:
-      threeFreq[cipherNS[i:i+3]] = 1
-  
-  return threeFreq
+    counter = 0
+    while counter != 1000:
+      # Switch 2 letters in key
+      rand1 = 0
+      rand2 = 0
+      while rand1 == rand2: #To make sure numbers are not the same
+        rand1 = random.randint(0,25)
+        rand2 = random.randint(0,25)
+      playKey = maxPlayKey[:] # Copy key by value
 
-# Function that checks on one letter clash, true if clash
-# mostFreqLetChar -> character of selected mostFreqLet
-# letCombChar -> character of selected letter combination
-def clashLetCheck(mostFreqLetChar, letCombChar):
-  # Check if letter clashes
-  if letCombChar not in subLetters.values(): # Check if letter not assigned in subLetters
-    if subLetters[mostFreqLetChar] != None: # If there is a letter assigned
-      if subLetters[mostFreqLetChar] != letCombChar: # If assigned letter does not match
-        return True # There is a clash
+      # Switch Letters
+      playKey[rand1], playKey[rand2] = playKey[rand2], playKey[rand1]
+
+      decrypted = decrypt(cipher, playKey)
+
+      score = scoring(decrypted)
+
+      if score > maxScore:
+        maxScore = score
+        maxDecrypted = decrypted
+        maxPlayKey = playKey
+        counter = 0
+      
+      counter = counter + 1
+    
+    if maxScore > overallMaxScore:
+      overallMaxScore = maxScore
+      overallMaxPlayKey = maxPlayKey
+      overallMessage = decrypt(cipher, overallMaxPlayKey)
+    
+    overallCounter = overallCounter + 1
+
+  stringAndKey = [overallMessage, overallMaxPlayKey]
+  return stringAndKey
+
+# Find key most filter
+def mostFit(caesar, hC):
+  if len(caesar[0]) < 95:
+    return caesar
+
+  cScore = 0
+  for i in range(len(caesar[0]) - 5):
+    if (d.check(caesar[0][i:i + 5])):
+      cScore = cScore + 1
+
+  hScore = 0
+  for i in range(len(hC[0]) - 5):
+    if (d.check(hC[0][i:i + 5])):
+      hScore = hScore + 1
+
+  if cScore >= hScore:
+    return caesar
   else:
-    if subLetters[mostFreqLetChar] != letCombChar:
-      return True # There is a clash with another letter assigned
+    return hC
 
-  return False
+
+# # Function that returns ordered dictionary of combination frequency by greatest to least
+# def orderLetFreqDict(letterFreq):
+#   incrNum = 1
+#   orderedDict = {}
+#   for i in reversed(sorted(letterFreq, key=letterFreq.get)):
+#     orderedDict[incrNum] = i
+#     incrNum = incrNum + 1
+
+#   return orderedDict
+
+# # Function that counts character frequency
+# def oneLetFreqCheck(cipherNS):
+#   charCount = {}
+#   for i in cipherNS:
+#     if (i in charCount):
+#       charCount[i] = charCount[i] + 1
+#     else:
+#       charCount[i] = 1
+#   return charCount
+
+# # Function that finds the most frequent two letters
+# def twoLetFreqCheck(cipherNS):
+#   threeFreq = {}
+#   for i in range(len(cipherNS)-2):
+#     if (cipherNS[i:i+2] in threeFreq):
+#       threeFreq[cipherNS[i:i+2]] = threeFreq[cipherNS[i:i+2]] + 1
+#     else:
+#       threeFreq[cipherNS[i:i+2]] = 1
+  
+#   return threeFreq
+
+# # Function that finds the most frequent three letters
+# def threeLetFreqCheck(cipherNS):
+#   threeFreq = {}
+#   for i in range(len(cipherNS)-2):
+#     if (cipherNS[i:i+3] in threeFreq):
+#       threeFreq[cipherNS[i:i+3]] = threeFreq[cipherNS[i:i+3]] + 1
+#     else:
+#       threeFreq[cipherNS[i:i+3]] = 1
+  
+#   return threeFreq
 
 # Cipher 1--------------------------------------------------------
 # length of alphabets is 26 for all for loops
-# brute force encrypted message 1
-bruteForceStorage1 = bruteForce(cipher1, key)
+cipher1NS = ""
+for i in cipher1:
+  if i != ' ':
+    cipher1NS = cipher1NS + i
 
-# finds correct key
-findCorrectKey(bruteForceStorage1)
+cipher1NS = cipher1NS.upper()
+
+print("------------------Cipher 1------------------")
+
+# brute force encrypted message 1
+bruteForceStorage1 = bruteForce(cipher1NS, bruteForceStorageKeys1, key)
+
+# finds best fit key
+bestFit1 = mostFit(findCorrectKey(bruteForceStorage1, bruteForceStorageKeys1), hillClimb(cipher1NS))
+
+print("Most fit key of cipher 1: " + str("".join(bestFit1[1])))
+print("Resulting sentence: " + str(bestFit1[0]))
 
 # Cipher 2--------------------------------------------------------
-#brute force encrypted message 2
-bruteForceStorage2 = bruteForce(cipher2, key)
+cipher2NS = ""
+for i in cipher2:
+  if i != ' ':
+    cipher2NS = cipher2NS + i
 
-# finds correct key
-findCorrectKey(bruteForceStorage2)
+cipher2NS = cipher2NS.upper()
+
+print("------------------Cipher 2------------------")
+
+#brute force encrypted message 2
+bruteForceStorage2 = bruteForce(cipher2.upper(), bruteForceStorageKeys2, key)
+
+# finds best fit key
+bestFit2 = mostFit(findCorrectKey(bruteForceStorage2, bruteForceStorageKeys2), hillClimb(cipher2NS))
+
+print("Most fit key of cipher 2: " + str("".join(bestFit2[1])))
+print("Resulting sentence: " + str(bestFit2[0]))
 
 # Cipher 3--------------------------------------------------------
 cipher3NS = ""
@@ -167,116 +262,37 @@ for i in cipher3:
   if i != ' ':
     cipher3NS = cipher3NS + i
 
-message3 = ""
+cipher3NS = cipher3NS.upper()
 
+print("------------------Cipher 3------------------")
 
-# Check most frequent letter in order from greatest to least
-orderedOneLetterFreq = orderLetFreqDict(oneLetFreqCheck(cipher3NS))
+#brute force encrypted message 3
+bruteForceStorage3 = bruteForce(cipher3.upper(), bruteForceStorageKeys3, key)
 
-# Check most frequent 2 letters in order from greatest to least
-orderedTwoLetterFreq = orderLetFreqDict(twoLetFreqCheck(cipher3NS))
+# finds best fit key
+bestFit3 = mostFit(findCorrectKey(bruteForceStorage3, bruteForceStorageKeys3), hillClimb(cipher3NS))
 
-# Check most frequent 3 letters in order from greatest to least
-orderedThreeLetterFreq = orderLetFreqDict(threeLetFreqCheck(cipher3NS))
-
-# Gets the most frequent 3 letter combination
-mostFreq3Let = orderedThreeLetterFreq[1]
-
-# check letter clash for e
-if not clashLetCheck(orderedOneLetterFreq[1], mostCommon[0]): # if not clash
-  subLetters[orderedOneLetterFreq[1]] = mostCommon[0] # q = e
-
-# check 3 letter clash for shq and the
-clashed = False
-for i in range(len(orderedThreeLetterFreq[1])):
-  if clashLetCheck(orderedThreeLetterFreq[1][i], threeComb[1][i]): # if clash
-    clashed = True
-    break
-  else: # No clash
-    print("False")
-
-
-if not clashed: # If no clashes
-  for i in range(len(orderedThreeLetterFreq[1])):
-    subLetters[orderedThreeLetterFreq[1][i]] = threeComb[1][i]
-
-for i in range(len(mostCommon)):
-  if mostCommon[i] not in subLetters.values(): # Continue if letter not assigned
-    for j in orderedOneLetterFreq: # Goes through each orderedOneLetterFreq
-      if not clashLetCheck(orderedOneLetterFreq[j], mostCommon[i]): # Continue if letter not clashing
-        subLetters[orderedOneLetterFreq[j]] = mostCommon[i]
-        break
-
-for i in range(len(cipher3NS)):
-  if subLetters[cipher3NS[i]] != None:
-    message3 = message3 + subLetters[cipher3NS[i]]
-  else:
-    message3 = message3 + cipher3NS[i]
-
-print(message3)
+print("Most fit key of cipher 3: " + str("".join(bestFit3[1])))
+print("Resulting sentence: " + str(bestFit3[0]))
 
 # Cipher 4--------------------------------------------------------
 # Cipher4 No Space
-
-# Reset substituted letters
-for i in subLetters:
-  subLetters[i] = None
-
 cipher4NS = ""
 for i in cipher4:
   if i != ' ':
     cipher4NS = cipher4NS + i
 
-message4 = ""
+print("------------------Cipher 4------------------")
 
+cipher4NS = cipher4NS.upper()
 
-# Check most frequent letter in order from greatest to least
-orderedOneLetterFreq = orderLetFreqDict(oneLetFreqCheck(cipher4NS))
+#brute force encrypted message 4
+bruteForceStorage4 = bruteForce(cipher4.upper(), bruteForceStorageKeys4, key)
 
-# Check most frequent 2 letters in order from greatest to least
-orderedTwoLetterFreq = orderLetFreqDict(twoLetFreqCheck(cipher4NS))
+# finds best fit key
+bestFit4 = mostFit(findCorrectKey(bruteForceStorage4, bruteForceStorageKeys4), hillClimb(cipher4NS))
 
-# Check most frequent 3 letters in order from greatest to least
-orderedThreeLetterFreq = orderLetFreqDict(threeLetFreqCheck(cipher4NS))
-
-print("")
-
-# Gets the most frequent 3 letter combination
-mostFreq3Let = orderedThreeLetterFreq[1]
-
-# check letter clash for e
-if not clashLetCheck(orderedOneLetterFreq[1], mostCommon[0]): # if not clash
-  subLetters[orderedOneLetterFreq[1]] = mostCommon[0] # q = e
-
-# check 3 letter clash for shq and the
-clashed = False
-for i in range(len(orderedThreeLetterFreq[1])):
-  if clashLetCheck(orderedThreeLetterFreq[1][i], threeComb[1][i]): # if clash
-    clashed = True
-    break
-  else: # No clash
-    print("False")
-
-
-if not clashed: # If no clashes
-  for i in range(len(orderedThreeLetterFreq[1])):
-    subLetters[orderedThreeLetterFreq[1][i]] = threeComb[1][i]
-
-for i in range(len(mostCommon)):
-  if mostCommon[i] not in subLetters.values(): # Continue if letter not assigned
-    for j in orderedOneLetterFreq: # Goes through each orderedOneLetterFreq
-      if not clashLetCheck(orderedOneLetterFreq[j], mostCommon[i]): # Continue if letter not clashing
-        subLetters[orderedOneLetterFreq[j]] = mostCommon[i]
-        break
-
-for i in range(len(cipher4NS)):
-  if subLetters[cipher4NS[i]] != None:
-    message4 = message4 + subLetters[cipher4NS[i]]
-  else:
-    message4 = message4 + cipher4NS[i]
-
-print(subLetters)
-
-print(message4)
+print("Most fit key of cipher 4: " + str("".join(bestFit4[1])))
+print("Resulting sentence: " + str(bestFit4[0]))
 
 
